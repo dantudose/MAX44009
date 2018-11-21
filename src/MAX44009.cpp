882e518c57c1ef360e6174e6751fa4e9d60beb4f
Dan Tudose <dantudose1984@gmail.com>
2018-11-21 22:18:13 +0200

    Update MAX44009.cpp

--- src/MAX44009.cpp
+++ src/MAX44009.cpp
@@ -15,6 +15,7 @@ int MAX44009::begin()
 float MAX44009::get_lux(void)
 {
 	unsigned int data[2];
+	
 	Wire.beginTransmission(MAX_ADDR);
 	Wire.write(0x03);
 	Wire.endTransmission();
@@ -25,8 +26,8 @@ float MAX44009::get_lux(void)
 	// Read 2 bytes of data luminance msb, luminance lsb
 	if (Wire.available() == 2)
 	{
-    	data[0] = Wire.read();
-    	data[1] = Wire.read();
+    		data[0] = Wire.read();
+    		data[1] = Wire.read();
 	}
  
 	// Convert the data to lux
